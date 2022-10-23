32859d0524f14c32a510d54fe5424e179f218cd0
Tedi Vyshka <tedi.vyshka@gmail.com>
2022-10-23 12:21:12 +0200

    Added solveur

--- dpll.ml
+++ dpll.ml
@@ -21,9 +21,9 @@ let filter_map filter list =
 let print_modele: int list option -> unit = function
   | None   -> print_string "UNSAT\n"
   | Some modele -> print_string "SAT\n";
-     let modele2 = sort (fun i j -> (abs i) - (abs j)) modele in
-     List.iter (fun i -> print_int i; print_string " ") modele2;
-     print_string "0\n"
+      let modele2 = sort (fun i j -> (abs i) - (abs j)) modele in
+      List.iter (fun i -> print_int i; print_string " ") modele2;
+      print_string "0\n"
 
 (* ensembles de clauses de test *)
 let exemple_3_12 = [[1;2;-3];[2;3];[-1;-2;3];[-1;-3];[1;-2]]
@@ -35,86 +35,105 @@ let coloriage = [[1;2;3];[4;5;6];[7;8;9];[10;11;12];[13;14;15];[16;17;18];[19;20
 
 (********************************************************************)
 
-(* simplifie : int -> int list list -> int list list 
+(* simplifie : int -> int list list -> int list list
    applique la simplification de l'ensemble des clauses en mettant
    le littéral l à vrai *)
- let rec supp_clause   i  claus = 
-          match claus with 
-            |[]->[]
-            |x::r->  if ( mem i x ) then supp_clause i r 
-                    else [x] @supp_clause i r ;; 
-          
-          
-         
-         let rec  recherche_et_suppression  i l = 
-           match l  with 
-            |[]-> []
-            |x::r -> [ let rec aux   i  claus = 
-            match claus with
-            |[]->[]
-            |x::r->   if (x=(-i)) then   aux i r 
-               else [x]@aux i r in aux i x] @ recherche_et_suppression  i r;;   
-            
-               
-            
-         let simplifie l clauses =
-          let nv_clause = supp_clause l  clauses in 
-          recherche_et_suppression l nv_clause ;;  
+
+let rec supp_clause i  claus =
+  match claus with
+  |[]->[]
+  |x::r->  if ( mem i x ) then supp_clause i r
+      else [x] @supp_clause i r ;;
+
+let rec  recherche_et_suppression  i l =
+  match l  with
+  |[]-> []
+  |x::r -> [ let rec aux i claus =
+               match claus with
+               |[]->[]
+               |x::r->   if (x=(-i)) then   aux i r
+                   else [x]@aux i r in aux i x] @ recherche_et_suppression  i r;;
+
+let simplifie l clauses =
+  let nv_clause = supp_clause l  clauses in
+  recherche_et_suppression l nv_clause ;;
+
+
 
 (* solveur_split : int list list -> int list -> int list option
    exemple d'utilisation de `simplifie' *)
-(* cette fonction ne doit pas être modifiée, sauf si vous changez 
+(* cette fonction ne doit pas être modifiée, sauf si vous changez
    le type de la fonction simplifie *)
 let rec solveur_split clauses interpretation =
   (* l'ensemble vide de clauses est satisfiable *)
   if clauses = [] then Some interpretation else
   (* un clause vide n'est jamais satisfiable *)
   if mem [] clauses then None else
-  (* branchement *) 
-  let l = hd (hd clauses) in
-  let branche = solveur_split (simplifie l clauses) (l::interpretation) in
-  match branche with
-  | None -> solveur_split (simplifie (-l) clauses) ((-l)::interpretation)
-  | _    -> branche
+  (* branchement *)
+    let l = hd (hd clauses) in
+    let branche = solveur_split (simplifie l clauses) (l::interpretation) in
+    match branche with
+    | None -> solveur_split (simplifie (-l) clauses) ((-l)::interpretation)
+    | _    -> branche
 
 (* tests *)
 (* let () = print_modele (solveur_split systeme []) *)
 (* let () = print_modele (solveur_split coloriage []) *)
 
 (* solveur dpll récursif *)
-    
+
 (* unitaire : int list list -> int
     - si `clauses' contient au moins une clause unitaire, retourne
-      le littéral de cette clause unitaire ;
-    - sinon, lève une exception `Not_found' *)
-let unitaire clauses =
-  (* à compléter *)
-  0
-    
+le littéral de cette clause unitaire ;
+- sinon, lève une exception `Not_found' *)
+
+let rec unitaire clauses =
+  match clauses with
+  | []-> raise Not_found
+  | [x]::_ -> x
+  | h::t -> unitaire t
+;;
+
 (* pur : int list list -> int
     - si `clauses' contient au moins un littéral pur, retourne
       ce littéral ;
     - sinon, lève une exception `Failure "pas de littéral pur"' *)
- let rec  is_pur l1 l2 = 
-   match l1 with  
-  |[]-> failwith  "NO PUR LITTERAL" 
-  |x::r->   if (not (mem (-x) l2)   ) then x else           
-          is_pur r l2;;        
-
 
 let pur clauses =
- let l = List.flatten(clauses)  in 
-     is_pur l l ;; 
+  let l = List.flatten(clauses) in
+  let rec  is_pur l1 l2 =
+    match l1 with
+    |[]-> failwith "pas_de_litteral_pur"
+    |x::r->   if (not (mem (-x) l2)   ) then x else
+          is_pur r l2 in
+  is_pur l l;;
 
-(* solveur_dpll_rec : int list list -> int list -> int list option *)
+(*solveur_dpll_rec : int list list -> int list -> int list option *)
 let rec solveur_dpll_rec clauses interpretation =
-  (* à compléter *)
-  None
+  let pur = try(Some (pur clauses)) with
+      pas_de_litteral_pur -> None in
+  let unit = try(Some(unitaire clauses)) with
+      Not_found -> None in
+  match (unit,pur) with
+  | (Some litteral, _) | (_, Some litteral) -> solveur_dpll_rec (simplifie litteral clauses) (litteral::interpretation)
+  | _ -> match clauses with
+      [[]] -> None
+      |[]-> Some interpretation
+    | h::t ->
+        match h with
+          [] -> None
+        | h::rest ->
+            let cnf1 = simplifie h clauses in
+            let cnf2 = simplifie (-h) clauses in
+            let try1 = solveur_dpll_rec cnf1 (h::(rest@interpretation)) in
+            if(try1 = None) then solveur_dpll_rec cnf2 ((-h)::(rest@interpretation))
+            else try1
+
 
 (* tests *)
-(* let () = print_modele (solveur_dpll_rec systeme []) *)
-(* let () = print_modele (solveur_dpll_rec coloriage []) *)
-
+(*let () = print_modele (solveur_dpll_rec systeme [])
+let () = print_modele (solveur_dpll_rec coloriage [])
+*)
 let () =
   let clauses = Dimacs.parse Sys.argv.(1) in
   print_modele (solveur_dpll_rec clauses [])
